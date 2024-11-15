07e93e29f4e1b9e94a6508594395f17a1d495cfd
marouan boumchahate <marouanboumchahate@gmail.com>
2024-11-15 18:13:03 +0300

    Change the database configuration

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:%40marouan%2FNARJIS%2F08%40@localhost/TodoApplicationDatabase'
+#SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://todoapp_q80j_user:94omzdh4w0Pp8W9Ym3OmliLFJ4wexGVu@dpg-csrm7a0gph6c73b8v9eg-a.frankfurt-postgres.render.com/todoapp_q80j'
 
 
 #engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) --> Second argument only for sqlite3
